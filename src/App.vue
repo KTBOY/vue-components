fa58b4c6057ee31461165c8cb5061afebb7f34bb
舒克 <1435787623@qq.com>
2025-03-24 17:51:10 +0800

    fix:

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+  import { RouterLink, RouterView } from 'vue-router';
+  import HelloWorld from './components/HelloWorld.vue';
+  import TheWelcome from './components/TheWelcome.vue';
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <!-- <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink> -->
+        <TheWelcome />
+      </nav>
+    </div>
+  </header>
+
+  <div class="router-view">
+    <div class="two-view"> 
+       <RouterView />
+     </div>
+  </div>
+</template>
+
+<style scoped>
+  .wrapper {
+    /*  */
+    /* background: #e0e0e0; */
+    height: 500px;
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+  .router-view {
+    background-color: #f5f5f5;
+    padding: 5px;
+    width: 100%;
+  }
+  .two-view{
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+    padding: 5px;
+  }
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,25 @@
+<!--
+ * @Author: zlc
+ * @Date: 2025-03-20 15:23:28
+ * @LastEditTime: 2025-03-24 11:04:06
+ * @LastEditors: zlc
+ * @Description: 
+ * @FilePath: \vue-components\vueAntComponents\src\views\AboutView.vue
+-->
+<template>
+  <div class="about">
+    <DrawRect>
+      <div>
+        <img :src="faceImage"/>
+      </div>
+    </DrawRect>
+  </div>
+</template>
+<script lang="ts" setup>
+import DrawRect from "../components/DrawRect/index.vue"
+import faceImage from "@/assets/img/face.png"
+</script>
+
+<style>
+
+</style>
